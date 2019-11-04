36dc7d43d16f1f533c83af3f40d29162c2703b3a
andriilham <andr3yfcb@gmail.com>
2019-11-04 08:17:30 +0700

    Ganti nama dikit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,4 +1,4 @@
 <?php $this->load->view("modul/headadm"); ?>
 
-<h1 class="nama">Andri Ilham</h1>
+<h1 class="nama">Andri</h1>
 <a href="<?php echo base_url("Myadmin/tambahdata"); ?>">Tambah Data</a>
